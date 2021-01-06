35a339ef5be57a14e168481718b7ea93ff431cf2
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-01-06 18:34:59 -0500

    added event listeners - code along

--- app.js
+++ app.js
@@ -3,3 +3,25 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+// console.log('Hello World!')
+
+
+//add a button
+//when the user clicks the button, change the style and text content of the welcomeElement
+
+
+const welcomeElement = document.getElementById('welcome')
+console.log(welcomeElement);
+
+const myButton = document.getElementById('change-button');
+
+myButton.addEventListener('click', () => {
+    console.log('they clicked!!');
+    welcomeElement.style.color = 'magenta';
+    welcomeElement.style.background = 'lightgreen';
+
+    welcomeElement.textContent = 'I CAN DO ANYTHING!!';
+});
+
+
