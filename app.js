4b1fb10c0846c0af44fdf4b658a2f45f9fcfa32a
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-01-07 12:03:32 -0500

    updates

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 // import functions and grab DOM elements
+const welcomeElement = document.getElementById('welcome');
+const myButton = document.getElementById('banana-button');
+const fruitsElement = document.getElementById('whats-that');
+
+const input = document.getElementById('user-input');
+
 
 // initialize state
 
 // set event listeners to update state and DOM
 
-// console.log('Hello World!')
+myButton.addEventListener('click', () => {
+    console.log('more bananas!!');
 
+    fruitsElement.style.background = 'yellow';
+    fruitsElement.style.color = 'orange';
+    fruitsElement.textContent = 'bananas - thats the code word Jimmy';
+
+    welcomeElement.textContent = input.Value;
+
+
+});
+
+//Banana Ipsum by Jed Hartman https://www.kith.org/jed/2001/09/25/immortal-bananas/
+// console.log('Hello World!')
 
 //add a button
 //when the user clicks the button, change the style and text content of the welcomeElement
-
-
-const welcomeElement = document.getElementById('welcome')
-console.log(welcomeElement);
-
-const myButton = document.getElementById('change-button');
-
-myButton.addEventListener('click', () => {
-    console.log('they clicked!!');
-    welcomeElement.style.color = 'magenta';
-    welcomeElement.style.background = 'lightgreen';
-
-    welcomeElement.textContent = 'I CAN DO ANYTHING!!';
-});
-
 
